1cbc1cfa072d47de8749d2d4b011456c00544a83
Shreyas Kulkarni <shreyaskulkarni202@gmail.com>
2022-11-11 02:55:18 +0530

    Get Metadata from header

--- adalight_ambilight_rp2040.ino
+++ adalight_ambilight_rp2040.ino
@@ -34,12 +34,18 @@ volatile uint8_t magicWord[] = {'A', 'd', 'a'};
 volatile uint8_t hi, lo, chk, i;
 uint32_t last_time = 0;
 volatile uint16_t howManyLED = 0;
-
+volatile byte r, g, b;
 void setup() {
-  Serial.begin(115200);
   strip.begin();
   strip.clear();
   strip.show();
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(230400);
+  Serial.setTimeout(20);
+  //  while (!Serial) {
+  //    delay(100);
+  //    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  //  }
   LED_Demo();
 }
 
@@ -49,35 +55,35 @@ void loop() {
     isBufferReady = false;
     strip.show();
   } else {
-    if (current_time - last_time >= 1000) {
+    if (current_time - last_time >= 3000) {
       // Send ACK packet every second
       Serial.print("Ada\n");
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
       last_time = current_time;
     }
   }
 }
 
 void LED_Demo() {
-  for (int i = 0; i <= 64; i++) {
-    strip.rainbow();
+  uint8_t color_pos = 0;
+  uint32_t tempColor;
+  uint16_t tempPixel;
+  strip.setBrightness(100);
+  randomSeed(analogRead(0));
+  for (uint32_t i = 0; i < NUMBER_OF_LEDS; i++) {
+    strip.setPixelColor(i, colorWheel(color_pos));
+    strip.show();
+    color_pos += 256 / NUMBER_OF_LEDS;
+    delayMicroseconds(1500000 / NUMBER_OF_LEDS);
+  }
+  for (int i = 100; i >= 0; i--) {
     strip.setBrightness(i);
     strip.show();
-    delay(25);
+    delay(20);
   }
-  delay(1000);
-  for (int i = 64; i > 0; i--) {
-    strip.rainbow();
-    strip.setBrightness(i);
-    strip.show();
-    delay(25);
-  }
+  strip.setBrightness(100);
   strip.clear();
   strip.show();
-}
-
-inline uint8_t getByteFromSerial() {
-  while (!Serial.available()) ;;
-  return (uint8_t)Serial.read();
 }
 
 void serialEvent() {
@@ -85,38 +91,65 @@ void serialEvent() {
   while (Serial.available()) {
     if (magicWord[i] == (uint8_t)Serial.read()) {
       i++;
+      Serial.println("ACKRX");
       if (i == 3) {
         // Magic packet matched, now buf the header
         // ref: https://www.partsnotincluded.com/visualizing-adalight-header-information/
-        hi = getByteFromSerial();
-        lo = getByteFromSerial();
-        chk = getByteFromSerial();
 
+        Serial.println("ACKOK");
+        while (!Serial.available()) ;;
+        hi = Serial.read();
+        while (!Serial.available()) ;;
+        lo = Serial.read();
+        while (!Serial.available()) ;;
+        chk = Serial.read();
+        Serial.println("HEDOK");
+        // Verify Checksum
         if (chk != (hi ^ lo ^ 0x55)) {
           break;
         }
         // Extract metadata from header
-        howManyLED = (256L * (long)hi + (long)lo + 1L);
-        for (uint8_t i = 0; i < NUMBER_OF_LEDS; i++) {
+        howManyLED = ((hi << 8) + lo + 1);
+        Serial.println("CHKOK");
+        Serial.print("NUMLED:"); Serial.println(howManyLED);
+        for (uint16_t i = 0; i < NUMBER_OF_LEDS; i++) {
           byte r, g, b;
           if (i < howManyLED) {
-            r = getByteFromSerial();
-            g = getByteFromSerial();
-            b = getByteFromSerial();
+            Serial.print("LED:"); Serial.println(i);
+            while (!Serial.available());
+            r = Serial.read();
+            while (!Serial.available());
+            g = Serial.read();
+            while (!Serial.available());
+            b = Serial.read();
             strip.setPixelColor(i, r, g, b);
           } else {
             strip.setPixelColor(i, 0, 0, 0);
           }
         }
         isBufferReady = true;
+        Serial.println("ALLOK");
         Serial.flush();
       }
     } else {
-      strip.clear();
-      strip.show();
+      Serial.println("ERR");
+      //      strip.clear();
+      //      strip.show();
       Serial.flush();
       break;
     }
   }
+}
 
+uint32_t colorWheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
